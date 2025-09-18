e1a494494b5f21e5d1e6f412f4b9d3293f9db425
JakubKw19 <kkwiecie@interia.pl>
2025-09-18 11:01:08 +0200

    added timelline

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -1,8 +1,51 @@
+import { Timeline } from "@/components/ui/timeline";
 import {
   Typography,
   TypographyH1,
   TypographyH2,
 } from "@/components/ui/typography/Typography";
+
+const data = [
+  {
+    title: "03.2025 – 06.2025",
+    content: (
+      <section>
+        <TypographyH2>Intern Fullstack Developer</TypographyH2>
+        <TypographyH2 className="italic font-medium">UnitUnicorn</TypographyH2>
+        <Typography>
+          Developing a web application using React.js and Django. Implementing
+          components with TypeScript and Styled Components. Using Material UI
+          library for design. Creating and integrating with REST API.
+          Collaborating in a team using Jira and Git.
+        </Typography>
+      </section>
+    ),
+  },
+  {
+    title: "11.2020 – 12.2020",
+    content: (
+      <section>
+        <TypographyH2>Intern IT service manager</TypographyH2>
+        <TypographyH2 className="italic font-medium">
+          EXPANDER Integrator IT Sp. z o.o
+        </TypographyH2>
+        <Typography>Assisted with network infrastructure tasks.</Typography>
+      </section>
+    ),
+  },
+  {
+    title: "03.2020 – 04.2020",
+    content: (
+      <section>
+        <TypographyH2>Intern IT service manager</TypographyH2>
+        <TypographyH2 className="italic font-medium">
+          EXPANDER Integrator IT Sp. z o.o
+        </TypographyH2>
+        <Typography>Assisted with network infrastructure tasks.</Typography>
+      </section>
+    ),
+  },
+];
 
 function Experience() {
   return (
@@ -10,49 +53,7 @@ function Experience() {
       <TypographyH1 className="mb-10 flex justify-center">
         Work Experience
       </TypographyH1>
-      <ul className="space-y-7 w-4/5 mx-auto">
-        <li>
-          <span className="flex justify-between">
-            <TypographyH2>Intern Fullstack Developer</TypographyH2>
-            <TypographyH2 className="font-light">
-              03.2025 – 06.2025
-            </TypographyH2>
-          </span>
-          <TypographyH2 className="italic font-medium">
-            UnitUnicorn
-          </TypographyH2>
-          <Typography>
-            Developing a web application using React.js and Django. Implementing
-            components with TypeScript and Styled Components. Using Material UI
-            library for design. Creating and integrating with REST API.
-            Collaborating in a team using Jira and Git.
-          </Typography>
-        </li>
-        <li>
-          <span className="flex justify-between">
-            <TypographyH2>Intern IT service manager</TypographyH2>
-            <TypographyH2 className="font-light">
-              11.2020 – 12.2020
-            </TypographyH2>
-          </span>
-          <TypographyH2 className="italic font-medium">
-            EXPANDER Integrator IT Sp. z o.o
-          </TypographyH2>
-          <Typography>Assisted with network infrastructure tasks</Typography>
-        </li>
-        <li>
-          <span className="flex justify-between">
-            <TypographyH2>Intern IT service manager</TypographyH2>
-            <TypographyH2 className="font-light">
-              03.2020 – 04.2020
-            </TypographyH2>
-          </span>
-          <TypographyH2 className="italic font-medium">
-            EXPANDER Integrator IT Sp. z o.o
-          </TypographyH2>
-          <Typography>Assisted with network infrastructure tasks</Typography>
-        </li>
-      </ul>
+      <Timeline data={data} />
     </div>
   );
 }
